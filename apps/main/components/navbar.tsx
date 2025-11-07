4a58d467fceff1fb0d504bcd67092c4c709bd201
kelpexternal-ai <kelp.external@gmail.com>
2025-11-06 16:55:03 -0800

    Fix DisclosurePanel Fragment error in production

--- apps/main/components/navbar.tsx
+++ apps/main/components/navbar.tsx
@@ -50,27 +50,29 @@ function MobileNavButton() {
 function MobileNav() {
   return (
     <DisclosurePanel className="lg:hidden">
-      <div className="flex flex-col gap-6 py-4">
-        {links.map(({ href, label }, linkIndex) => (
-          <motion.div
-            initial={{ opacity: 0, rotateX: -90 }}
-            animate={{ opacity: 1, rotateX: 0 }}
-            transition={{
-              duration: 0.15,
-              ease: 'easeInOut',
-              rotateX: { duration: 0.3, delay: linkIndex * 0.1 },
-            }}
-            key={href}
-          >
-            <Link href={href} className="text-base font-medium text-gray-950">
-              {label}
-            </Link>
-          </motion.div>
-        ))}
-      </div>
-      <div className="absolute left-1/2 w-screen -translate-x-1/2">
-        <div className="absolute inset-x-0 top-0 border-t border-black/5" />
-        <div className="absolute inset-x-0 top-2 border-t border-black/5" />
+      <div>
+        <div className="flex flex-col gap-6 py-4">
+          {links.map(({ href, label }, linkIndex) => (
+            <motion.div
+              initial={{ opacity: 0, rotateX: -90 }}
+              animate={{ opacity: 1, rotateX: 0 }}
+              transition={{
+                duration: 0.15,
+                ease: 'easeInOut',
+                rotateX: { duration: 0.3, delay: linkIndex * 0.1 },
+              }}
+              key={href}
+            >
+              <Link href={href} className="text-base font-medium text-gray-950">
+                {label}
+              </Link>
+            </motion.div>
+          ))}
+        </div>
+        <div className="absolute left-1/2 w-screen -translate-x-1/2">
+          <div className="absolute inset-x-0 top-0 border-t border-black/5" />
+          <div className="absolute inset-x-0 top-2 border-t border-black/5" />
+        </div>
       </div>
     </DisclosurePanel>
   )
